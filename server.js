eae0711c48d1962a65128f96a8059a2130195c3f
Mikkal24 <voltx180@gmail.com>
2017-04-15 11:23:58 -0700

    more server changes

--- server.js
+++ server.js
@@ -7,13 +7,29 @@ var PORT = 3000;
 
 app.use(bodyParser.json());
 
+//Reservations
+var reservations = [{
+	customerName: "test",
+	phoneNumber: "test#",
+	customerEmail: "testEmail",
+	customerID: "testID"
 
-
+}]
+//Waitlist
+var waitlist = [{
+	customerName: "test",
+	phoneNumber: "test#",
+	customerEmail: "testEmail",
+	customerID: "testID"
+}]
 
 //Home page request
 app.get("/", function(req,res){
 	res.sendFile(path.join(__dirname, "home.html"));
 });
+
+// Routes
+// =============================================================
 
 //Reservation page request
 app.get("/reserve", function(req,res){
@@ -27,8 +43,18 @@ app.get("/tables", function(req,res){
 
 //API call to get the tables
 app.get("/api/:tables?", function(req,res){
-	return res.json(tables);
+	return res.json(reservations);
 });
+
+app.get("/api/:waitlist?", function(req,res){
+	resturn res.json(waitlist);
+});
+
+app.post("/api/new", function(req,res){
+	var newReservation = req.body;
+
+	res.json(newReservation);
+})
 
 //Listener
 app.listen(PORT, function(){
