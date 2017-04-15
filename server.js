4de19a87eaa26679b98cd2bdd069084611ca5158
Mikkal24 <voltx180@gmail.com>
2017-04-15 11:10:45 -0700

    made some small changes to server

--- server.js
+++ server.js
@@ -5,7 +5,33 @@ var path = require('path');
 var app = express();
 var PORT = 3000;
 
+app.use(bodyParser.json());
 
+
+
+
+//Home page request
+app.get("/", function(req,res){
+	res.sendFile(path.join(__dirname, "home.html"));
+});
+
+//Reservation page request
+app.get("/reserve", function(req,res){
+	res.sendFile(path.join(__dirname,"reserve.html"));
+});
+
+//view table page request
+app.get("/tables", function(req,res){
+	res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+//API call to get the tables
+app.get("/api/:tables?", function(req,res){
+	return res.json(tables);
+});
+
+//Listener
 app.listen(PORT, function(){
 	console.log("App listening on PORT "+PORT);
-})
+});
+
