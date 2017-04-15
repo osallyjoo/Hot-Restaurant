aa1062cbe3e68d83bd321a4471ab4dbb3c6d36a1
marco alvarado <Marco@marcos-MacBook-Pro.local>
2017-04-15 11:30:41 -0700

    moved code

--- server.js
+++ server.js
@@ -62,20 +62,3 @@ app.listen(PORT, function(){
 });
 
 
- // Takes values from  the forn
-    $("#submitReservation").on("click", function(event) {
-      event.preventDefault();
-      var newReservation = {
-        name: $("#name").val().trim(),
-        phoneNumber: $("#phoneNumber").val().trim(),
-        email: $("#email").val().trim(),
-        uniqueId: $("#uniqueId").val().trim()
-      };
-
-      // POSTs to server
-      $.post("/api/reserve", newReservation)
-      .done(function(data) {
-        console.log(data);
-        alert("Adding Reservartion...");
-      });
-    });
