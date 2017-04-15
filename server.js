4db68cdde7c3771ff06beaf4fe395c92f681858f
Mikkal24 <voltx180@gmail.com>
2017-04-15 10:56:37 -0700

    added server file

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var PORT = 3000;
+
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT "+PORT);
+})
